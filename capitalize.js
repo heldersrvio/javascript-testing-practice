6c77ceaa8fab69d3595b942360ffc2ebb803da02
Helder Sérvio <heldersrvio@gmail.com>
2020-07-16 21:10:55 -0300

    First tests added

---
+++ capitalize.js
@@ -0,0 +1,7 @@
+const capitalize = (string) => {
+    return (string.length === 0)
+        ? string.toUpperCase()
+        : string[0].toUpperCase() + string.slice(1);
+};
+
+export default capitalize;
---
+++ capitalize.test.js
@@ -0,0 +1,9 @@
+import capitalize from './capitalize';
+
+test('Capitalize works (1)', () => {
+    expect(capitalize('hello world')).toBe('Hello world');
+});
+
+test('Capitalize works (2)', () => {
+    expect(capitalize('Again!')).toBe('Again!');
+});
